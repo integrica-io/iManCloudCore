f65f17adf9617c19612d23326c9ee9329d3b145d
coeuskk <krupinski.kamil@gmail.com>
2026-05-19 23:05:36 +0100

    added methods to revoke client access token and token as a parameter.

--- auth/revokeAccessToken.go
+++ auth/revokeAccessToken.go
@@ -6,20 +6,19 @@ import (
 	"strings"
 	"net/http"
 	"net/url"
-
 	"iManCloudCore/internal"
 )
 
-func (c *Client)revokeClientAccessToken(ctx context.Context) error {
+func (c *Client) RevokeClientAccessToken(ctx context.Context) error {
 	endpoint := c.BaseUrl.JoinPath("auth","oauth2","revoke-token")
-	data := url.Values{}
 
-	data.Add("access_token", c.AccessToken)
-	
-	if c.AccessToken == ""{
+	if c.Token == nil{
 		return fmt.Errorf("no access token available to revoke")
 	}
 
+	data := url.Values{}
+	data.Add("access_token", c.Token.AccessToken)
+	
 	reqClient := &http.Client{}
 	req, err := http.NewRequestWithContext(ctx, "POST", endpoint.String(), strings.NewReader(data.Encode()))
 	if err != nil {
@@ -39,5 +38,32 @@ func (c *Client)revokeClientAccessToken(ctx context.Context) error {
 		return fmt.Errorf("%s", errMessage)
 	}
 
+	return nil
+}
+
+func (c *Client) RevokeAccessToken(ctx context.Context, accessToken string) error {
+	endpoint := c.BaseUrl.JoinPath("auth","oauth2","revoke-token")
+	data := url.Values{}
+
+	data.Add("access_token", accessToken)
+
+	reqClient := &http.Client{}
+	req, err := http.NewRequestWithContext(ctx, "POST", endpoint.String(), strings.NewReader(data.Encode()))
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := reqClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	okResp := resp.StatusCode >= 200 || resp.StatusCode < 300
+	if !okResp{
+		errMessage := internal.HttpErrorHandler(resp)
+ 		return fmt.Errorf("%s", errMessage)
+	}
 	return nil
 }
