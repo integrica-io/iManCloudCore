4d6791e1b43639a9f3a15d7356c28fff8cb114dc
coeuskk <krupinski.kamil@gmail.com>
2026-05-23 13:30:56 +0100

    Ping endpoint, Remove headers and added context to request builder

--- customers/ping.go
+++ customers/ping.go
@@ -11,15 +11,13 @@ func PingServer(ctx context.Context, c *internal.Client) error {
 	endpoint := c.BaseUrl.JoinPath("work", "api", "v2", "customers", c.TokenCfg.CustomerId, "ping")
 	
 	req := internal.HttpRequestBuilder{}
-	
-	headers := make(map[string]string)
-	headers["X-Auth-Token"] = c.Token.AccessToken
 		
-	req.Url(*endpoint).Method(internal.Get).Headers(headers)
+	req.Url(*endpoint).Method(internal.Get).Context(ctx)
 
 	if err := req.Exec(); err != nil {
 		slog.Error("error","error", err)
 		os.Exit(1)
 	}
+	
 	return nil	
 }
