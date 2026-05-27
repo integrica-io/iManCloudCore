c0b8064a2a6db49281ac2f3567245801fba89997
coeuskk <krupinski.kamil@gmail.com>
2026-05-27 21:24:16 +0100

    added Get Active External Auth Type function

---
+++ auth/getActiveExternalAuthType.go
@@ -0,0 +1,25 @@
+package auth
+
+import (
+	"context"
+	"iManCloudCore/internal"
+)
+
+type GetActiveExternalAuthTypeOutput struct {
+	Data struct {
+		ID string `json:"id"`
+	} `json:"data"`
+}
+
+func GetActiveExternalAuthType(ctx context.Context, client *internal.Client)(GetActiveExternalAuthTypeOutput, error){
+	endpoint := client.BaseUrl.JoinPath("platform", "api", "v2","customers",client.TokenCfg.CustomerId, "settings", "system", "auth", "external-auth-type")
+	var data GetActiveExternalAuthTypeOutput
+
+	req := internal.HttpRequestBuilder{}
+	req.Context(ctx).Url(*endpoint).Method(internal.Get).ToJson(&data)
+
+	if err := client.Req(req); err != nil {
+		return data, err
+	}
+	return data, nil
+}
