c3d21815f4f0bbfe360c2620afc1b64ec24e4063
coeuskk <krupinski.kamil@gmail.com>
2026-06-05 10:51:58 +0100

    Create GetGlobalGroupsHashes.go

---
+++ customers/GetGlobalGroupsHashes.go
@@ -0,0 +1,45 @@
+package customers
+
+import (
+	"context"
+	"iManCloudCore/internal"
+
+	"github.com/google/go-querystring/query"	
+)
+
+type GetGlobalGroupsHashesOutput struct {
+	Data []struct {
+		DirectoryID   string `json:"directory_id"`
+		DsHash        string `json:"ds_hash"`
+		DsMembersHash string `json:"ds_members_hash"`
+		Enabled       bool   `json:"enabled"`
+	} `json:"data"`
+	Cursor string `json:"cursor"`
+}
+
+type GetGlobalGroupsHashesOptions struct{
+	Cursor string `url:"cursor,omitempty"`
+	Limit int `url:"limit,omitempty"`
+}
+
+func GetGlobalGroupsHashes(ctx context.Context, client *internal.Client, options *GetGlobalRolesOptions)(GetGlobalGroupsHashesOutput, error){
+
+	var data GetGlobalGroupsHashesOutput
+	endpoint := client.BaseUrl.JoinPath("work","api","v2","customers",client.TokenCfg.CustomerId, "directory-sync", "groups", "hashes")
+
+	if options != nil {
+		values, err := query.Values(options)
+		if err != nil {
+			return data, err
+		}
+		endpoint.RawQuery = values.Encode()
+	}
+
+	req := internal.HttpRequestBuilder{}
+	req.Context(ctx).Url(*endpoint).Method(internal.Get).ToJson(&data)
+
+	if err := client.Req(req); err != nil {
+		return data, err
+	}
+	return data, nil
+}
