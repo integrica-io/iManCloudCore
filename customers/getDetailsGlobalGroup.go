86eeb2a8b91a2f61e6265eec2ff5d1c7b03af697
coeuskk <krupinski.kamil@gmail.com>
2026-06-05 16:53:59 +0100

    Create getDetailsGlobalGroup.go

---
+++ customers/getDetailsGlobalGroup.go
@@ -0,0 +1,39 @@
+package customers
+
+import (
+	"time"
+	"context"
+	"iManCloudCore/internal"
+)
+
+type GetDetailsGlobalGroupOutput struct {
+	Data struct {
+		CreateDate        time.Time `json:"create_date"`
+		DirectoryID       string    `json:"directory_id"`
+		DistinguishedName string    `json:"distinguished_name"`
+		EditDate          time.Time `json:"edit_date"`
+		Enabled           bool      `json:"enabled"`
+		FullName          string    `json:"full_name"`
+		GlobalID          int64     `json:"global_id"`
+		GroupDomain       string    `json:"group_domain"`
+		GroupNos          int       `json:"group_nos"`
+		GroupNumber       int64     `json:"group_number"`
+		ID                string    `json:"id"`
+		IsExternal        bool      `json:"is_external"`
+		LastSyncTs        time.Time `json:"last_sync_ts"`
+	} `json:"data"`
+}
+
+func GetDetailsGlobalGroup(ctx context.Context, client *internal.Client, groupId string)(GetDetailsGlobalGroupOutput, error){
+
+	var data GetDetailsGlobalGroupOutput
+	endpoint := client.BaseUrl.JoinPath("work","api","v2","customers",client.TokenCfg.CustomerId, "groups", groupId)
+
+	req := internal.HttpRequestBuilder{}
+	req.Context(ctx).Url(*endpoint).Method(internal.Get).ToJson(&data)
+
+	if err := client.Req(req); err != nil {
+		return data, err
+	}
+	return data, nil
+}
