7f5666de372599d937e8d7293a6a065b54363cf9
coeuskk <krupinski.kamil@gmail.com>
2026-06-05 10:48:49 +0100

    Create getGlobalUsersHases.go

---
+++ customers/getGlobalUsersHases.go
@@ -0,0 +1,44 @@
+package customers
+
+import (
+	"context"
+	"iManCloudCore/internal"
+
+	"github.com/google/go-querystring/query"	
+)
+
+type GetGlobalUsersHashesOutput struct {
+	Data []struct {
+		DirectoryID string `json:"directory_id"`
+		DsHash      string `json:"ds_hash"`
+		AllowLogon  bool   `json:"allow_logon"`
+	} `json:"data"`
+	Cursor string `json:"cursor"`
+}
+
+type GetGlobalUsersHashesOptions struct{
+	Cursor string `url:"cursor,omitempty"`
+	Limit int `url:"limit,omitempty"`
+}
+
+func GetGlobalUsersHashes(ctx context.Context, client *internal.Client, options *GetGlobalRolesOptions)(GetGlobalUsersHashesOutput, error){
+
+	var data GetGlobalUsersHashesOutput
+	endpoint := client.BaseUrl.JoinPath("work","api","v2","customers",client.TokenCfg.CustomerId, "directory-sync", "users", "hashes")
+
+	if options != nil {
+		values, err := query.Values(options)
+		if err != nil {
+			return data, err
+		}
+		endpoint.RawQuery = values.Encode()
+	}
+
+	req := internal.HttpRequestBuilder{}
+	req.Context(ctx).Url(*endpoint).Method(internal.Get).ToJson(&data)
+
+	if err := client.Req(req); err != nil {
+		return data, err
+	}
+	return data, nil
+}
