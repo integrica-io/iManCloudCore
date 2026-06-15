369316a966e9c4214dfc9478fbaca5150e02e80a
coeuskk <krupinski.kamil@gmail.com>
2026-06-15 22:32:34 +0100

    Create getUsersAccessWorkspace.go

---
+++ library/getUsersAccessWorkspace.go
@@ -0,0 +1,27 @@
+package library
+
+import (
+	"context"
+	
+	"github.com/integrica-io/iManCloudCore/internal"
+	"github.com/integrica-io/iManCloudCore/client"
+)
+
+func GetUsersAccessWorkspace(ctx context.Context, client client.Client, libraryId string, workspaceId string, userId string)(GetUsersAccessWorkspaceOutput, error){
+	var data GetUsersAccessWorkspaceOutput
+	endpoint := client.BaseUrl.JoinPath("work","api","v2","customers",client.TokenCfg.CustomerId, "libraries", libraryId, "workspaces", workspaceId, "users", userId, "security")
+
+	req := internal.HttpRequestBuilder{}
+	req.Context(ctx).Url(*endpoint).Method(internal.Get).ToJson(&data)
+
+	if err := client.Req(req); err != nil {
+		return data, err
+	}
+	return data, nil
+}
+
+type GetUsersAccessWorkspaceOutput struct {
+	Data struct {
+		Access string `json:"access"`
+	} `json:"data"`
+}
